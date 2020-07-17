7112b0cef130cace2edc92382abc4a8c00df09cc
bharat_patil <patilbharat09@gmail.com>
2020-07-17 15:55:07 +0530

    Update new schema

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const graphqlHttp = require("express-graphql").graphqlHTTP;
-const { buildSchema } = require("graphql");
+const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLID,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLSchema,
+} = graphql;
 
 const app = express();
 
@@ -9,121 +17,114 @@ const products = [
   {
     id: 1,
     name: "Nike Renew Run Running Shoes",
-    price: 1200.50,
-    categoryId: 2                     // Product belongs to category(ID)
+    price: 1200.5,
+    categoryId: 2, // Product belongs to category(ID)
   },
   {
     id: 2,
     name: "Graphic Print Men Round Neck Brown T-Shirt",
-    price: 640.50,
-    categoryId: 1
+    price: 640.5,
+    categoryId: 1,
   },
   {
     id: 3,
     name: "Nike Runallday Running Shoes",
-    price: 900.50,
-    categoryId: 2
+    price: 900.5,
+    categoryId: 2,
   },
   {
     id: 4,
     name: "Skinny Men Blue Jeans",
-    price: 700.50,
-    categoryId: 1
-  }
+    price: 700.5,
+    categoryId: 1,
+  },
 ];
-
-
 
 // Dummy Categories
 const categories = [
   {
     id: 1,
-    name: "Cloth"
+    name: "Cloth",
   },
   {
     id: 2,
-    name: "Footwear"
-  }
-]
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  ## START: Product - define product schema
-  type Product {
-    id: ID!
-    name: String!
-    price: Float!
-  }
-  ## END: Product
-
-  ## START: Category - define category schema
-  type Category {
-    id: ID!
-    name: String!
-    products: [Product!]!
-  }
-  ## END: Category
-
-  type Queries { 
-    ## products - query return array of product                       
-    products: [Product!]!
-    ## categories - query return array of category with its products                       
-    categories: [Category!]!
-  }
-
-  ## Add ProductInput schema
-  ## START: ProductInput - define ProductInput schema
-  input ProductInput {
-    name: String!
-    price: Float!
-  }
-  ## END: ProductInput
-
-  ## Add Mutations schema
-  type Mutations {
-    addProduct(productInput: ProductInput): Product
-  }
-
-  ## Add mutation in below schema
-  schema {
-    query: Queries
-    mutation: Mutations
-  }
-`);
-
-// Return products with category id
-const productsByCategory = (id) => {
-  return products.filter(product =>  product.categoryId === id );
-}
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  products: () => {           // products -- resolver function return list of products
-    return products;
+    name: "Footwear",
   },
-  categories: () => {         // categories -- resolver function return list of category with products
-    categories.map(category => {
-      category['products'] = productsByCategory(category.id);
-    })
-    return categories;
-  },
-  addProduct: (args) => {     // addProduct -- resolver function return new product object
-    const product = {
-      id: Math.floor(Math.random() * 100).toString(),
-      name: args.productInput.name,
-      price: +args.productInput.price
+];
+
+ProductType = new GraphQLObjectType({
+  // Schema for product
+  name: "Product",
+  fields: () => ({
+    id: { type: GraphQLID }, // Type of variable
+    name: { type: GraphQLString },
+    price: { type: GraphQLFloat },
+  }),
+});
+
+CategoryType = new GraphQLObjectType({    // Schema for category
+  name: "Category",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    products: {                           
+      type: new GraphQLList(ProductType),  // Return list of product for parent category id
+      // Child Resolver function get parent category values in 'parent' variable
+      resolve: (parent, args, context) => {
+        return products.filter(product =>  product.categoryId === parent.id );
+      },
     }
-    products.push(product);
-    return product;
+  })
+});
+
+const Mutations = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addProduct: {                         // Add product mutation
+      type: ProductType,                  // Type define response schema type 
+      args: {                             // args contains no of inputs to resolver function
+        name: { type: GraphQLString },    // input variable with data type
+        price: { type: GraphQLFloat }
+      },
+      resolve: (parent, args, context) => {   // Rsolver function for addProduct mutation
+        const product = {
+          id: Math.floor(Math.random() * 100).toString(),
+          name: args.name,                     // Get input parameter in `args`
+          price: +args.price
+        }
+        products.push(product);             // Add new product in products
+        return product;                     // Return newly contracted product
+      },
+    }
+  }
+});
+
+const Queries = new GraphQLObjectType({
+  name: "QueriesType", // GraphQL object name
+  fields: {
+    products: {
+      type: new GraphQLList(ProductType), // products - return list of product
+      resolve: () => {
+        return products;
+      },
+    },
+    categories: {
+      type: new GraphQLList(CategoryType), // products - return list of category
+      resolve: () => {
+        return categories;
+      },
+    },
   },
-};
+});
 
 app.use(
   "/graphql",
   graphqlHttp({
-    schema: schema,     // query + mutation
-    rootValue: root,    // Resolver function for each API endpoint
-    graphiql: true,     // true - use the GraphiQL tool
+    schema: new GraphQLSchema({
+      query: Queries, // All queries
+      mutation: Mutations // All mutations
+    }),
+    graphiql: true, // true - use the GraphiQL tool
   })
 );
 
